158b42cefb921747c4e471897a3c455ca605ed66
yagna <mannamyagneswari@gmail.com>
2026-02-28 09:27:22 +0530

    javascript completed

---
+++ javascript/script.js
@@ -0,0 +1,58 @@
+console.log("Hello World");
+// var can be change  ,const cannot be change i must be intilized it on that time only,let cannot be intilized 
+// var a;
+// a=10;
+// var a;
+// a=30;
+// let b=90;
+// b=78;
+// const c=09;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// var num=10;
+// var str="yagna";//`` '' ""
+// var bool=true;
+// var un;
+// var nu=null;
+// var big=123454567774n;
+// var symbol=Symbol('li');
+// console.log(typeof num) 
+// console.log(typeof str) 
+// console.log(typeof bool) 
+// console.log(typeof un) 
+// console.log(typeof nu) 
+// console.log(typeof big)
+// console.log(typeof symbol)  
+
+// var arr=[1,2,3,4,5]
+// console.log(arr) 
+// console.log(typeof arr) 
+
+// var obj={
+//     name:"yagna",
+//     dept:"DS"
+// }
+// console.log(obj);
+// console.log(typeof obj) 
+
+//arith
+var a=10;
+var b="20";
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a%b);
+console.log(a*b);
+console.log(a**b);
+
+//relational
+var a=10;
+var b="10";
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a<=b);
+console.log(a>=b);
+
